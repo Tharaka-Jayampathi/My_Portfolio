6dd5569372bebafcb65dfeadfb7832f0e34cab87
Tharaka-Jayampathi <jayampathitharaka@gmail.com>
2026-06-12 00:50:49 +0530

    edit profile

--- src/component/profile.js
+++ src/component/profile.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import './profile.css';
-
+import image1 from './Image1.jpeg';
 
 export default function profile() {
   return (
     <Container>
       <Row>
         <Col xs={12} className="profile-align-right">
-          <Image src="/Image1.jpeg" roundedCircle className="profile-picture" />
+          <Image src={image1} roundedCircle className="profile-picture" />
         </Col>
       </Row>
     </Container>
